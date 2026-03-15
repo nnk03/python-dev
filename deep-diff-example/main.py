45bb4324aeff6b9717fd2c5237e23ee87d24bbf3
Neeraj Krishna N <neerajnjps03@gmail.com>
2026-03-15 11:41:04 +0530

    updated DeepDiff example

--- deep-diff-example/main.py
+++ deep-diff-example/main.py
@@ -1,31 +1,37 @@
 from deepdiff import DeepDiff
 
 old_config = {
-    'A':{
-        'A-set': [12, 13, 14]
+    'A': {
+        'A-set': [12, 13, 14],
+        'B-set': [12, 13, 14]
     }
 }
 
 new_config = {
-    'A':{
-        'A-set': [13, 14, 12]
+    'A': {
+        'A-set': [13, 14, 12], # Order changed, should report a diff
+        'B-set': [13, 14, 12], # Order changed, should be ignored
     }
 }
 
-def check_a_set_order( path ):
-    return 'A-set' in path
+def check_a_set_order(level):
+    # If 'A-set' is in the path, return False (do NOT ignore order)
+    # For everything else (like B-set), return True (ignore order)
+    print(level.path(),'--> ignore order : ', 'A-set' not in level.path())
+    return 'A-set' not in level.path()
 
 def main():
-    print("Hello from deep-diff-example!")
-    diffWithOrder = DeepDiff( old_config, new_config )
-    print( diffWithOrder.pretty() )
+    # diffWithOrder = DeepDiff( old_config, new_config )
+    # print( diffWithOrder.pretty() )
+    #
+    # diffWithoutOrder = DeepDiff( old_config, new_config,ignore_order=True )
+    # print( diffWithoutOrder.pretty() )
 
-    diffWithoutOrder = DeepDiff( old_config, new_config,ignore_order=True )
-    print( diffWithoutOrder.pretty() )
-
-    diffWithParticularOrder = DeepDiff( old_config, new_config, ignore_order_func=check_a_set_order)
-    print( diffWithParticularOrder.pretty() )
-
+    # ignore_order_func tells DeepDiff WHERE to ignore order.
+    # We want it to ignore order ONLY if it's NOT A-set.
+    diff = DeepDiff(old_config, new_config, ignore_order_func=check_a_set_order)
+    
+    print(diff.pretty())
 
 if __name__ == "__main__":
     main()
